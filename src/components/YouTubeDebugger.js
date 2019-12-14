40b5d08462a6bc9a191ba120a6ea4c14f5ecc60c
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-14 10:38:40 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -18,7 +18,7 @@ export default class YouTubeDebugger extends React.Component{
     handleClickBitrate = ()=>{
       this.setState({
         settings: {
-          ...this.state.
+          ...this.state.settings ,bitrate: 12 
         }
       })
     }
